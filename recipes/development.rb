ad266e7ee167730b187312d0b7100e7f8e923c16
dexter505 <dexter505@users.noreply.github.com>
2015-08-03 04:16:13 -0700

    Update development.rb

--- recipes/development.rb
+++ recipes/development.rb
@@ -1,20 +1,20 @@
 # Since Vagrant on an NTFS host doesn't support symlinks properly
-directory "/vagrant/node_modules" do
+directory "/var/www/condor/node_modules" do
     recursive true
     action :delete
-    not_if "test -L /vagrant/node_modules"
-    only_if { node['vagrant']['symlink_npm'] }
+    not_if "test -L /var/www/condor/node_modules"
+    only_if { node['laravel']['symlink_npm'] }
 end
 
 directory "/tmp/node_modules" do
     owner "vagrant"
     group "vagrant"
-    only_if { node['vagrant']['symlink_npm'] }
+    only_if { node['laravel']['symlink_npm'] }
 end
 
-link "/vagrant/node_modules" do
+link "/var/www/condor/node_modules" do
     owner "vagrant"
     group "vagrant"
     to "/tmp/node_modules"
-    only_if { node['vagrant']['symlink_npm'] }
+    only_if { node['laravel']['symlink_npm'] }
 end
