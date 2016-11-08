3d10e76adec82748f8f65602cd07f67d88682127
Dexter <dexter505@users.noreply.github.com>
2016-11-07 18:01:53 -0800

    Update packages.rb

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -8,6 +8,18 @@
 # Makes sure apt is up to date
 include_recipe "apt"
 
+# Add repositories
+apt_repository "php" do
+	uri "http://ppa.launchpad.net/ondrej/php"
+	distribution node['lsb']['codename']
+	components ["main"]
+	keyserver "keyserver.ubuntu.com"
+	key "E5267A6C"
+end
+
+# Makes sure apt is up to date
+include_recipe "apt"
+
 # Install packages
 node['laravel']['packages'].each do |a_package|
   package a_package
