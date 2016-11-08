0214ea91eaa8272b36a3033b58460499bce767fe
Dexter <dexter505@users.noreply.github.com>
2016-11-07 18:32:46 -0800

    Update packages.rb

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -9,16 +9,16 @@
 include_recipe "apt"
 
 # Add repositories
-apt_repository "php" do
-	uri "http://ppa.launchpad.net/ondrej/php"
-	distribution node['lsb']['codename']
-	components ["main"]
-	keyserver "keyserver.ubuntu.com"
-	key "E5267A6C"
-end
+#apt_repository "php" do
+#	uri "http://ppa.launchpad.net/ondrej/php"
+#	distribution node['lsb']['codename']
+#	components ["main"]
+#	keyserver "keyserver.ubuntu.com"
+#	key "E5267A6C"
+#end
 
 # Makes sure apt is up to date
-include_recipe "apt"
+#include_recipe "apt"
 
 # Install packages
 node['laravel']['packages'].each do |a_package|
@@ -29,6 +29,8 @@ bash "install php and modules" do
   user "root"
   cwd "/var/www/#{node['laravel']['name']}"
   code <<-EOH
+  add-apt-repository ppa:ondrej/php
+  apt-get update
   apt-get install php5.6 php5.6-dev php5.6-cli php-pear php5.6-apcu php5.6-mysql php5.6-curl php5.6-mcrypt php5.6-memcached php5.6-gd php5.6-json
   EOH
 end
